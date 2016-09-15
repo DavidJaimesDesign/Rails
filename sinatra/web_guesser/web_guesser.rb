a78ec512dc6332fa2936a20d695b4983dbb702ed
David Jaimes <davidjaimesdesign@gmail.com>
2016-09-15 16:52:30 -0700

    finished number guessing portion

--- sinatra/web_guesser/web_guesser.rb
+++ sinatra/web_guesser/web_guesser.rb
@@ -1,7 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
-rand_no = rand(100)
+SECRET_NO = rand(100)
 
 get '/' do
-	erb :index, :locals => {:rand_no => rand_no}
+	guess = params['guess']
+	message = check_guess(guess)
+	erb :index, :locals => {:message => message}#, :guess_to_high
+end
+
+def check_guess(guess)
+	if guess.to_i < SECRET_NO
+		"TO LOW BRO"
+	elsif  guess.to_i > SECRET_NO
+		"TO DAMN HIGH"
+	elsif guess.to_i == SECRET_NO
+		"CORRECT YOU MAY LIVE ANOTHER DAY the number was #{SECRET_NO}"
+	end
 end

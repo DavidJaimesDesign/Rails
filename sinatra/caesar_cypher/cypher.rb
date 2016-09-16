3ff050891b16265b0f188ab03b9dc05e0cdec3dd
David Jaimes <davidjaimesdesign@gmail.com>
2016-09-16 16:05:05 -0700

    Linked CSS to the rails app time to make it look pretty

--- sinatra/caesar_cypher/cypher.rb
+++ sinatra/caesar_cypher/cypher.rb
@@ -7,7 +7,7 @@ get '/' do
 	shift  = params["shift"]
 	enc_string = cypher(string, shift)
 	"cypher goes here"
-	erb :index, :locals => {:enc_string => enc_string}
+	erb :index, :locals => {:enc_string => enc_string} 
 end
 
 def cypher(t = "",s = 0)
@@ -27,15 +27,5 @@ def cypher(t = "",s = 0)
 end
 
 =begin
-return "Error max shift is 26" if s > 26 
-	@t = t.to_s
-	@s = s.to_i
-	ascii = t.chars.map {|c| c.ord}
-	shifted = ascii.map  do |c| if c == 32
-									c
-								else
-									c + @s
-								end
-						end
-	return shifted.map {|c| c.chr}.join
+<div>Icons made by <a href="http://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 =end

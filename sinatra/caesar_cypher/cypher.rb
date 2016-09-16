e9a302236997d42955fec6ca0e42a7b158e141be
David Jaimes <davidjaimesdesign@gmail.com>
2016-09-16 14:55:13 -0700

    Cypher v 0.1 complete HTML skeleton and Cypher work. v 1.0 is with css styling

--- sinatra/caesar_cypher/cypher.rb
+++ sinatra/caesar_cypher/cypher.rb
@@ -10,8 +10,20 @@ get '/' do
 	erb :index, :locals => {:enc_string => enc_string}
 end
 
-def cypher(t = " ",s = 0)
-	return t
+def cypher(t = "",s = 0)
+	c_string = ""
+	t = t.to_s
+	s = s.to_i
+	#return "Error max shift is 26" if s > 26 
+	ascii = t.chars.map {|c| c.ord}
+	shifted = ascii.map  do |c| if c == 32
+									c
+								else
+									c + s
+								end
+						end
+	c_string = shifted.map {|c| c.chr}.join
+	return c_string.to_s
 end
 
 =begin

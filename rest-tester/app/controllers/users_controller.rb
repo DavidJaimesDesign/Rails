dcce4df6f1bc1fb7ed342b3e72a7cf4ec3d61886
David Jaimes <davidjaimesdesign@gmail.com>
2016-09-28 05:34:19 -0700

    Warmup functioning moving on to chapter 5

--- rest-tester/app/controllers/users_controller.rb
+++ rest-tester/app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
 	def new
 		@new = "need to research this"
 	end
+
+	def posts
+		@posts = "I am a post"
+	end
+
+	def update
+		@update = "I am an update"
+	end
+
+	def delete
+		@delete = "I am a delete"
+	end
 end
